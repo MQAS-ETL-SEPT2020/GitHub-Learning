bc6e19823c8843bf3d81e22a0b7fb8655942c851
User <User@192.168.1.41>
2022-09-25 12:05:54 +0530

    Updated Automation TestScript on TC_02_Preeti_Test

--- GitHub__Learning/src/test/java/tools_qa/TC_02_Preeti_Test.java
+++ GitHub__Learning/src/test/java/tools_qa/TC_02_Preeti_Test.java
@@ -1,34 +1,37 @@
 package tools_qa;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.Reporter;
-import org.testng.annotations.Test;
 
-import mqasetl.GitHub__Learning.Basetest;
 
-public class TC_02_Preeti_Test extends Basetest {
-	@Test
-	public void button() {
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.interactions.Actions;
+	import org.testng.Reporter;
+	import org.testng.annotations.Test;
 
-		driver.get("https://demoqa.com/buttons");
-		Reporter.log("Chrome driver launched successfully", true);
+	import mqasetl.GitHub__Learning.Basetest;
 
-		Actions act = new Actions(driver);
-		Reporter.log("Created object of action class and passing the driver instance", true);
+	public class TC_02_Preeti_Test extends Basetest {
+		@Test
+		public void button() {
 
-		WebElement double1 = driver.findElement(By.id("doubleClickBtn"));
-		act.doubleClick(double1).perform();
-		Reporter.log("Successfully clicked on double click ", true);
+			driver.get("https://demoqa.com/buttons");
+			Reporter.log("Chrome driver launched successfully", true);
 
-		WebElement rightClick = driver.findElement(By.id("rightClickBtn"));
-		act.contextClick(rightClick).perform();
-		Reporter.log("Successfully clicked on right click ", true);
+			Actions act = new Actions(driver);
+			Reporter.log("Created object of action class and passing the driver instance", true);
 
-		WebElement click = driver.findElement(By.xpath("//button[text()='Click Me']"));
-		act.click();
-		Reporter.log("Successfully clicked on click me", true);
-	}
+			WebElement double1 = driver.findElement(By.id("doubleClickBtn"));
+			act.doubleClick(double1).perform();
+			Reporter.log("Successfully clicked on double click ", true);
 
+			WebElement rightClick = driver.findElement(By.id("rightClickBtn"));
+			act.contextClick(rightClick).perform();
+			Reporter.log("Successfully clicked on right click ", true);
+
+			WebElement click = driver.findElement(By.xpath("//button[text()='Click Me']"));
+			act.click();
+			Reporter.log("Successfully clicked on click me", true);
+		}
+
+	
 }

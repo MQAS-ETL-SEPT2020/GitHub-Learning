e461276dd228efefd6cdea983d581627a2ef62e1
Anilkumarsm28 <anilkumarsm28@gmail.com>
2022-09-25 10:52:37 +0530

    updated test script TC_06_Anil_Test.java

--- GitHub__Learning/src/main/java/mqasetl/GitHub__Learning/App.java
+++
@@ -1,7 +0,0 @@
-package mqasetl.GitHub__Learning;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
--- GitHub__Learning/src/test/java/tools_qa/TC_06_Anil_Test.java
+++ GitHub__Learning/src/test/java/tools_qa/TC_06_Anil_Test.java
@@ -1,5 +1,22 @@
 package tools_qa;
 
-public class TC_06_Anil_Test {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
 
+import mqasetl.GitHub__Learning.Basetest;
+
+public class TC_06_Anil_Test extends Basetest{
+	@Test
+	public void testcase1() throws InterruptedException {
+		// opening demoQA page
+		driver.get("https://demoqa.com/");
+
+		// opening links page
+		driver.get("https://demoqa.com/links");
+		System.out.println("Links page opened successfully");
+
+		// clicking on "Home" link
+		driver.findElement(By.id("simpleLink")).click();
+		System.out.println("clicked on Home link susccessfully");
+	}
 }
